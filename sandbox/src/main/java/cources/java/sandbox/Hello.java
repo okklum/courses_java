a0027a9fa8c706abc2b233fc46971fd06ecfd4ff
okklum <okklum@gmail.com>
2016-04-10 15:33:22 +0300

    Форматирование кода по новым правилам (4 ->2)

--- sandbox/src/main/java/cources/java/sandbox/Hello.java
+++ sandbox/src/main/java/cources/java/sandbox/Hello.java
@@ -1,9 +1,9 @@
 package cources.java.sandbox;
 
 public class Hello {
-	
-	public static void main(String [] args){
-		System.out.println("Hello from the outside!");
-	}
-	
+
+  public static void main(String[] args) {
+    System.out.println("Hello from the outside!");
+  }
+
 }
