25549211978627f8cee2541d80369a093fc2b9a3
okklum <okklum@gmail.com>
2016-04-11 02:21:16 +0300

    Добавлено несколько функций в класс Hello

--- sandbox/src/main/java/cources/java/sandbox/Hello.java
+++ sandbox/src/main/java/cources/java/sandbox/Hello.java
@@ -3,7 +3,27 @@ package cources.java.sandbox;
 public class Hello {
 
   public static void main(String[] args) {
-    System.out.println("Hello from the outside!");
+    printText(" from the outside");
+    printText(", World");
+    printText(", Alisa");
+
+    int l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area (l));
+
+    double a = 4.5;
+    double b = 2.5;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
   }
 
+  public static void printText (String somebody){
+    System.out.println("Hello" + somebody + "!");
+  }
+
+  public static int area (int kvadr) {
+    return kvadr*kvadr;
+  }
+
+  public static double area (double a, double b){
+    return a * b;
+  }
 }
