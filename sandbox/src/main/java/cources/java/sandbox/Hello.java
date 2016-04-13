353f4efed0adf63eb6980cb6032021fedfe2ec82
okklum <okklum@gmail.com>
2016-04-14 01:57:31 +0300

    Задание №2 (1-3)
    Домашнее Задание №2, пункты 1-3 (промежуточный коммит, пока без
    конструктора)

--- sandbox/src/main/java/cources/java/sandbox/Hello.java
+++ sandbox/src/main/java/cources/java/sandbox/Hello.java
@@ -4,26 +4,34 @@ public class Hello {
 
   public static void main(String[] args) {
     printText(" from the outside");
-    printText(", World");
-    printText(", Alisa");
 
-    int l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " равна " + area (l));
+    //ДЗ №2: Создаем новые объекты
+    Point p1 = new Point();
+    Point p2 = new Point();
 
-    double a = 4.5;
-    double b = 2.5;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
+    //ДЗ №2: Добавляем координаты, пока без конструктора
+    p1.x = 2;
+    p1.y = 4;
+    p2.x = 3;
+    p2.y = 5;
+    //ДЗ №2: Выводим результат
+    System.out.println("Расстояние между точками равно " + distance(p1,p2));
+
+
+    //Создан объект с добавлением нового значения, атрибуты заполнены в конструкторе
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.kvadr + " равна " + s.area ());
+
+    Rectangle r = new Rectangle(4.5, 2.5);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
+  }
+
+  //ДЗ №2: Пишем функцию для вычисления
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
   }
 
   public static void printText (String somebody){
     System.out.println("Hello" + somebody + "!");
-  }
-
-  public static int area (int kvadr) {
-    return kvadr*kvadr;
-  }
-
-  public static double area (double a, double b){
-    return a * b;
   }
 }
---
+++ sandbox/src/main/java/cources/java/sandbox/Point.java
@@ -0,0 +1,11 @@
+package cources.java.sandbox;
+
+/**
+ * Created by alisa on 13.04.2016.
+ */
+public class Point {
+
+  //ДЗ №2: Прописываем атрибуты, которые соответствуют координатам точки на плоскости
+  public double x;
+  public double y;
+  }
