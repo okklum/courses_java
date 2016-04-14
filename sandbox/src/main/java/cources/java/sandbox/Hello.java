7db95a59ccbc8b5d8fb6a9fba7156597bce84a21
okklum <okklum@gmail.com>
2016-04-14 23:00:25 +0300

    Задание №2 (1-3), добавлен конструктор

--- sandbox/src/main/java/cources/java/sandbox/Hello.java
+++ sandbox/src/main/java/cources/java/sandbox/Hello.java
@@ -6,16 +6,11 @@ public class Hello {
     printText(" from the outside");
 
     //ДЗ №2: Создаем новые объекты
-    Point p1 = new Point();
-    Point p2 = new Point();
+    Point p1 = new Point(2,4);
+    Point p2 = new Point(3,5);
 
-    //ДЗ №2: Добавляем координаты, пока без конструктора
-    p1.x = 2;
-    p1.y = 4;
-    p2.x = 3;
-    p2.y = 5;
     //ДЗ №2: Выводим результат
-    System.out.println("Расстояние между точками равно " + distance(p1,p2));
+    System.out.println("Расстояние между точками с координатами " + p1.x + " , " + p1.y + " и " + p2.x + " , " + p2.y + " = " + distance(p1,p2));
 
 
     //Создан объект с добавлением нового значения, атрибуты заполнены в конструкторе
@@ -26,8 +21,9 @@ public class Hello {
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
   }
 
+
   //ДЗ №2: Пишем функцию для вычисления
-  public static double distance(Point p1, Point p2) {
+  public static double distance (Point p1, Point p2) {
     return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
   }
 
--- sandbox/src/main/java/cources/java/sandbox/Point.java
+++ sandbox/src/main/java/cources/java/sandbox/Point.java
@@ -8,4 +8,12 @@ public class Point {
   //ДЗ №2: Прописываем атрибуты, которые соответствуют координатам точки на плоскости
   public double x;
   public double y;
+
+  //ДЗ №2: Добавляем конструктор
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+
   }
+}
+
