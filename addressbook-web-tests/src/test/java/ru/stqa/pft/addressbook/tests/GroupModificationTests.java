0ad34e9408473055ce654f33f4ad96f24102234a
okklum <okklum@gmail.com>
2016-04-21 19:11:25 +0300

    Добавлен GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by alisa on 21.04.2016.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModofication () {
+
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test11", "test22", "test33"));
+    app.getGroupHelper().GroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
