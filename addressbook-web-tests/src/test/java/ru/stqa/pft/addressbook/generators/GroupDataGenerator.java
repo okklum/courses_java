8d57250c18485bd0e69e6b509214395be78eaa1d
okklum <okklum@gmail.com>
2016-05-17 01:45:10 +0300

    Подключена библиотека jCommander
    6.3 Указание опций командной строки в конфигурации теста

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -14,17 +17,38 @@ import java.util.List;
  */
 public class GroupDataGenerator {
 
-  public static void main(String[] args) throws IOException {
-    //передаем параметрами кол-во групп и путь к файлу
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
 
+  @Parameter(names = "-f", description = "Target file")
+  //библиотека jcommander не поддерживает напрямую работу с файлами, поэтому String
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    //создаем объект текущего класса
+    GroupDataGenerator generator = new GroupDataGenerator();
+    /* generator - объект, в к-ром заполн-ся соотв.атрибуты; args - параметры из командной строки
+    *    new JCommander(generator,args);
+     *    расписываем метод подробнее на случай косяка с указанием параметров*/
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    }catch (ParameterException e) {
+      //Выводим инфу о доступных опциях при неправильном запуске
+      jCommander.usage();
+      return;
+    }
+    
+    generator.run();
+  }
+
+  private void run() throws IOException {
     List<GroupData> groups = generateGroups(count);
-    save(groups, file);
+    save(groups, new File(file));
   }
 
   //Сохранение данных в файл
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void save(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (GroupData group: groups) {
@@ -35,7 +59,7 @@ public class GroupDataGenerator {
   }
 
   //Генерация данных
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
       groups.add(new GroupData().withName(String.format("test %s", i))
