be6684a982133142a645ff052b236194b3bbbb71
okklum <okklum@gmail.com>
2016-05-20 12:03:05 +0300

    Промежуточный коммит
    Добавлен датапровайдер Json для контактов, данные не забираются пока

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,9 +1,12 @@
 package ru.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 import java.io.File;
+
+@XStreamAlias("contacts")
 
 public class ContactData {
   @XStreamOmitField
