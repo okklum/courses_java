a0b3ce4bdb6351af29c31956ad9817dd93ab99bb
okklum <okklum@gmail.com>
2016-05-22 04:49:11 +0300

    Запись логов с помощью logback
    6.11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,24 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by alisa on 20.04.2016.
  */
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
 
   //объявляем переменную статической, чтобы избавиться от создания ее копий для разных тестовых классов
   //Set param for "browser" in edit configuration, else BrowserType as default
@@ -21,9 +31,19 @@ public class TestBase {
     app.init(app);
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + "with parameters" + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
   }
 
 }
