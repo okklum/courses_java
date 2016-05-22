3bdf8477cb1e95f93bf557da8560c5790de58ff9
okklum <okklum@gmail.com>
2016-05-22 17:15:19 +0300

    Получение данных из БД
    7.01 Добавлен драйвер базы данных для sql, прочитаны данные из БД
    (Connection-Statement-ResultSet)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.GroupSuite;
+
+import java.sql.*;
+
+/**
+ * Created by alisa on 22.05.2016.
+ */
+
+  public class DbConnectionTest {
+
+    @Test
+    public void testDbConnection() {
+      Connection conn = null;
+      try {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+        // Do something with the Connection
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
+        GroupSuite groups = new GroupSuite();
+        while (rs.next()) {
+          groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name")).
+                  withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+        }
+        rs.close();
+        st.close();
+        conn.close();
+
+        System.out.println(groups);
+        System.out.println("Групп всего " + groups.size());
+
+      } catch (SQLException ex) {
+        // handle any errors
+        System.out.println("SQLException: " + ex.getMessage());
+        System.out.println("SQLState: " + ex.getSQLState());
+        System.out.println("VendorError: " + ex.getErrorCode());
+      }
+    }
+  }
+
